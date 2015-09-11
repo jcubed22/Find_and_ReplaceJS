30c8c9b85e0bd279d83178b52b289e639f1b6370
Jordan Johansen <jordanjohan@gmail.com>
2015-09-11 10:27:51 -0700

    Create failing second test.

--- spec/specs.js
+++ spec/specs.js
@@ -2,4 +2,8 @@ describe('findReplace', function() {
     it("returns single letter user input without changing anything", function() {
         expect(findReplace("j", "j", "j")).to.equal("j");
     });
+
+    it("replaces single letter with another letter", function() {
+        expect(findReplace("j", "j", "z")).to.equal("z");
+    });
 });
