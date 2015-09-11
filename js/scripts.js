1080bff4926dde70024a1d319f91be0aeb39b5b5
Jordan Johansen <jordanjohan@gmail.com>
2015-09-11 12:02:17 -0700

    Fix some bugs in scripts.js to make page display.

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,21 @@ var findReplace = function(string, findWord, replaceWord) {
     var result = string.replace(regex, replaceWord);
     return result;
 };
+
+$(function() {
+    $("form#replacer").submit(function(event) {
+        var string = $("input#string").val();
+        var findWord = $("input#findWord").val();
+        var replaceWord = $("input#replaceWord").val();
+
+        var output = findReplace(string, findWord, replaceWord);
+
+        $(".input_string").empty();
+        $(".output").empty();
+        $(".input_string").text(string);
+        $(".output").text(output);
+        $("#result").show();
+        event.preventDefault();
+        debugger;
+    });
+});
