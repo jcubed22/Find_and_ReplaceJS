21ac9e5b40e55045af66fa483b9d7dce560b9361
Jordan Johansen <jordanjohan@gmail.com>
2015-09-11 10:26:03 -0700

    Create passing first test.

--- js/scripts.js
+++ js/scripts.js
@@ -1,55 +1,5 @@
-var pigLatin = function(string) {
-  var word_array = wordSeparator(string);
-  var returned_string = "";
-  word_array.forEach(function(word) {
-    returned_string += pigLatinWord(yChecker(word));
-  });
-  return returned_string.trim();
+var findReplace = function(string, findWord, replaceWord) {
+    var regex = new RegExp(findWord + 'gi');
+    string.replace(regex, replaceWord);
+    return string;
 };
-
-var pigLatinWord = function(word) {
-  for(var i=0; i<word.length; i++) {
-    word = quChecker(word);
-    var vowel = /[aeiouy]/g;
-    var currentLetter = word.charAt(0);
-    if (currentLetter.match(vowel)) {
-      break;
-    } else {
-      word = word.slice(1, word.length);
-      word += currentLetter;
-    }
-  }
-  return word += "ay ";
-}
-
-var wordSeparator = function(string) {
-  return string.split(" ");
-}
-
-var quChecker = function(string) {
-  if (string.charAt(0).toLowerCase() === "q" && string.charAt(1).toLowerCase() === "u") {
-    string = string.slice(2, string.length);
-    string += "qu";
-  }
-  return string;
-}
-
-var yChecker = function(string) {
-  if(string.charAt(0).toLowerCase() === "y") {
-    string = string.slice(1, string.length) + "y";
-  }
-  return string;
-}
-
-$(function() {
-  $("form#translator").submit(function(event) {
-    var string = $("input#string").val();
-    var result = pigLatin(string);
-    $(".output").empty();
-    $(".input_string").empty();
-    $(".input_string").text(string);
-    $(".output").text(result);
-    $("#result").show();
-    event.preventDefault();
-  });
-});
--- spec/specs.js
+++ spec/specs.js
@@ -1,1 +1,5 @@
-
+describe('findReplace', function() {
+    it("returns single letter user input without changing anything", function() {
+        expect(findReplace("j", "j", "j")).to.equal("j");
+    });
+});
