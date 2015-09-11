15fb5faba7c3ef852f2fd4a270e41f6e8e30876b
Jordan Johansen <jordanjohan@gmail.com>
2015-09-11 11:27:55 -0700

    Fix regular expression object to pass sixth text for excluding partial matches.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 var findReplace = function(string, findWord, replaceWord) {
-    var regex = new RegExp(findWord, 'g');
+    var regex = new RegExp("\\b" + findWord + "\\b", 'g');
     var result = string.replace(regex, replaceWord);
+    console.log(result);
     return result;
 };
